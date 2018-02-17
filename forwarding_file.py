cf4257a5ec6d33cb50eaae7ab707c3ee0d9edb8e
Nicholas Shahin <shahinna@umich.edu>
2018-02-17 14:48:51 -0500

    Added comments

--- forwarding_file.py
+++ forwarding_file.py
@@ -5,18 +5,20 @@ from gtts import gTTS
 
 def main(command):
 
-    print(command)
-
+    # Remove old audio files
     os.system("rm command.mp3")
     os.system("rm command.wav")
 
+    # Send command to google text to speech services, save as mp3
     command = "Alexa, " + command
     tts = gTTS(text=command, lang='en')
     tts.save("command.mp3")
 
+    # Convert command.mp3 to command.wav
     os.system("ffmpeg -i command.mp3 -ar 441000 -ac 2 command.wav")
 
+    # Make ssh request to amazon voice services
     # os.system("./request.sh")
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main("What time is it")

9527a1877fb04ca7d9859048101a47b2f0a4af6b
Xiaoran Xu <xiaorxu@umich.edu>
2018-03-20 08:16:46 -0400

    send command fixed

---
+++ send.js
@@ -0,0 +1,20 @@
+
+const shell = require('electron').shell;
+const exLinksBtn = document.getElementById('run');
+
+exLinksBtn.addEventListener('click', function (event) {
+  var command = document.getElementById("command").value;
+  const spawn = require('child_process').spawn;
+  const scriptExecution = spawn("python", ["test.py"]);
+
+          // Handle normal output
+          scriptExecution.stdout.on('data', (data) => {
+            console.log(String.fromCharCode.apply(null, data));
+          });
+
+          // Write data (remember to send only strings or numbers, otherwhise python wont understand)
+          var data = JSON.stringify(command);
+          scriptExecution.stdin.write(data);
+          // End data write
+          scriptExecution.stdin.end();
+        })

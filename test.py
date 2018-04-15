8f7d356dbceeb846c8cf043e2f7f49a8d5e5e95f
Xiaoran Xu <xiaorxu@umich.edu>
2018-04-15 13:38:58 -0400

    compound first push

--- test.py
+++ test.py
@@ -7,14 +7,14 @@ def read_in():
     return json.loads(lines[0])
 
 #do actual stuff here
-def main(cmd1, cmd2):
+def main(cmd1):
 
     '''
     this is just to show that python can read from the front end
     CHANGE CODE BELOW
     '''
-    webbrowser.open('https://www.google.com/' + cmd1 + '/' + cmd2)
+    webbrowser.open('https://www.google.com/' + cmd1)
 
 
 if __name__ == '__main__':
-    main(sys.argv[2], sys.argv[3])
+    main(sys.argv[1])

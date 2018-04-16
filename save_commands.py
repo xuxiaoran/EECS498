e5f7ede5c8f4356961c7567654ef887323bef23f
Nicholas Shahin <shahinna@umich.edu>
2018-04-16 17:17:46 -0400

    Converted compound commands to db

--- save_commands.py
+++ save_commands.py
@@ -65,37 +65,44 @@ def edit_command(command_id, input_string):
 # commands_list is a list of textual commands
 def save_compound_command(commands_list):
     #webbrowser.open('https://www.google.com/'+commands_list)
-    num_file = open("num_file", "r")
-    pos = num_file.read()
-    num_file.close()
-    pos = int(pos)
-    pos+=1
-    num_file = open("num_file", "w")
-    num_file.write(str(pos))
-    num_file.close()
+    conn = sqlite3.connect('saved_commands.db')
+    db = conn.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS compound_commands (cid integer primary key autoincrement, compound_command)")
+    cc = ""
+    for com in commands_list:
+        cc = cc + com + "|"
+    insert_command = "INSERT INTO compound_commands (command) VALUES (\"" + cc + "\")"
+    db.execute(insert_command)
 
-    filename = "compound_command_" + str(pos)
-    command_file = open("filename", "w")
-    command_file.writelines(commands_list)
-    command_file.close()
+    db.execute("SELECT MAX(cid) FROM compound_commands")
+    new_id = db.fetchone()
+    conn.commit()
+    conn.close()
+    output(str(new_id[0]))
 
 # Returns a list of lists of commands. The first thing in each list of commands is the id for that compound command
 def retrieve_compound_commands(compound_command_name):
-    commands_list = []
-    for possible_file in os.listdir("/"):
-        if possible_file.find("compound_command_") != -1:
-            pos = possible_file.replace("compound_command_", "")
-            command_file = open(possible_file, "r")
-            commands = command_file.readlines()
-            command_file.close()
-            commands.insert(0, pos)
-            commands_list.append(commands)
-
-    return commands_list
+    conn = sqlite3.connect('saved_commands.db')
+    db = conn.cursor()
+    db.execute("SELECT * FROM compound_commands")
+    data = db.fetchall()
+    dic = {}
+    for pair in data:
+        cid = pair[0]
+        commands = pair[1].split("|")
+        dic[str(cid)] = commands
+    conn.commit()
+    conn.close()
+    for key, value in dic.items():
+        output(str(key+'%'+value))
 
 def delete_compound_command(compound_command_id):
-    filename = "compound_command_" + str(compound_command_id)
-    os.remove(filename)
+    conn = sqlite3.connect('saved_commands.db')
+    db = conn.cursor()
+    delete_command = "DELETE FROM compound_commands WHERE cid = " + str(compound_command_id)
+    db.execute(delete_command)
+    conn.commit()
+    conn.close()
 
 
 if __name__ == '__main__':

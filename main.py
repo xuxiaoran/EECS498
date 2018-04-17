d3b770a44631a12da512d84b1e2ab80a1eec8efe
Xiaoran Xu <xiaorxu@umich.edu>
2018-04-16 23:46:04 -0400

    fix

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ from forwarding_file import send_command
 import sched, time
 
 def main(cmd):
-    lines = read_in()
-    line = ''
-    for item in lines:
-        line += item
-
     '''
     this is just to show that python can read from the front end
     CHANGE CODE BELOW
     '''
-    send_command(line)
+    send_command(cmd)
 
 if __name__ == '__main__':
     main(sys.argv[1])

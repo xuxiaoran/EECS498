4ba1168584cac698f3b2b0ac1eb00082ba60add7
javiercoUmich <javierco@umich.edu>
2018-04-17 00:06:59 -0400

    Added tutorial to the HTML

--- help.py
+++ help.py
@@ -1,7 +1,7 @@
 import os, sys
 
 def helpText():
-	os.system("osascript -e \'tell application \"Messages\" to send \"I am in trouble. Please send help!\" to buddy \"Amanda Williams\"\'")
+	os.system("osascript -e \'tell application \"Messages\" to send \"I am in trouble. Please send help!\" to buddy \"Jose Contreras\"\'")
 
 if __name__ == '__main__':
 	helpText()
